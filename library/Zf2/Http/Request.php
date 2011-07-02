a8e4355faeb7d98779089e97ba7d076bf3b74f3e
Rob Allen <rob@akrabat.com>
2011-07-02 10:38:27 +0100

    Implement Zf2\Http\Request's getBaseUrl() and modify getPathInfo() to take the base url into account

--- library/Zf2/Http/Request.php
+++ library/Zf2/Http/Request.php
@@ -232,7 +232,17 @@ class Request extends BaseRequest implements HttpRequest
 
     public function getPathInfo()
     {
-        return parse_url($this->getRequestUri(), PHP_URL_PATH);
+        $requestUri = $this->getRequestUri();
+        $pathInfo = $requestUri;
+
+        $baseUrl = $this->getBaseUrl();
+        if (!empty($baseUrl)) {
+            if (strpos($requestUri, $baseUrl) === 0) {
+                $pathInfo = substr($requestUri, strlen($baseUrl));
+            }
+        }
+
+        return parse_url($pathInfo, PHP_URL_PATH);
     }
 
 
@@ -243,6 +253,67 @@ class Request extends BaseRequest implements HttpRequest
 
     public function getBaseUrl()
     {
+        $filename = (isset($_SERVER['SCRIPT_FILENAME'])) ? basename($_SERVER['SCRIPT_FILENAME']) : '';
+
+        if (isset($_SERVER['SCRIPT_NAME']) && basename($_SERVER['SCRIPT_NAME']) === $filename) {
+            $baseUrl = $_SERVER['SCRIPT_NAME'];
+        } elseif (isset($_SERVER['PHP_SELF']) && basename($_SERVER['PHP_SELF']) === $filename) {
+            $baseUrl = $_SERVER['PHP_SELF'];
+        } elseif (isset($_SERVER['ORIG_SCRIPT_NAME']) && basename($_SERVER['ORIG_SCRIPT_NAME']) === $filename) {
+            $baseUrl = $_SERVER['ORIG_SCRIPT_NAME']; // 1and1 shared hosting compatibility
+        } else {
+            // Backtrack up the script_filename to find the portion matching
+            // php_self
+            $path    = isset($_SERVER['PHP_SELF']) ? $_SERVER['PHP_SELF'] : '';
+            $file    = isset($_SERVER['SCRIPT_FILENAME']) ? $_SERVER['SCRIPT_FILENAME'] : '';
+            $segs    = explode('/', trim($file, '/'));
+            $segs    = array_reverse($segs);
+            $index   = 0;
+            $last    = count($segs);
+            $baseUrl = '';
+            do {
+                $seg     = $segs[$index];
+                $baseUrl = '/' . $seg . $baseUrl;
+                ++$index;
+            } while (($last > $index) && (false !== ($pos = strpos($path, $baseUrl))) && (0 != $pos));
+        }
+
+        // Does the baseUrl have anything in common with the request_uri?
+        $requestUri = $this->getRequestUri();
+
+        if (0 === strpos($requestUri, $baseUrl)) {
+            // full $baseUrl matches
+            return $baseUrl;
+        }
+
+        if (0 === strpos($requestUri, dirname($baseUrl))) {
+            // directory portion of $baseUrl matches
+            $baseUrl = rtrim(dirname($baseUrl), '/');
+            return $baseUrl;
+        }
+
+        $truncatedRequestUri = $requestUri;
+        if (($pos = strpos($requestUri, '?')) !== false) {
+            $truncatedRequestUri = substr($requestUri, 0, $pos);
+        }
+
+        $basename = basename($baseUrl);
+        if (empty($basename) || !strpos($truncatedRequestUri, $basename)) {
+            // no match whatsoever; set it blank
+            $baseUrl = '';
+            return $baseUrl;
+        }
+
+        // If using mod_rewrite or ISAPI_Rewrite strip the script filename
+        // out of baseUrl. $pos !== 0 makes sure it is not matching a value
+        // from PATH_INFO or QUERY_STRING
+        if ((strlen($requestUri) >= strlen($baseUrl))
+            && ((false !== ($pos = strpos($requestUri, $baseUrl))) && ($pos !== 0)))
+        {
+            $baseUrl = substr($requestUri, 0, $pos + strlen($baseUrl));
+        }
+
+        return $baseUrl;
     }
 
     public function getScriptName()
